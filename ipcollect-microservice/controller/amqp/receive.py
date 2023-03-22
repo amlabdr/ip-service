ca7a1fa7ca50d3752bb6ec649517856818b9c54a
mhenimerz <hani.merzouki@gmail.com>
2023-03-22 08:36:00 -0400

    add amqp event receiver

--- ipcollect-microservice/controller/amqp/receive.py
+++ ipcollect-microservice/controller/amqp/receive.py
@@ -14,36 +14,36 @@ class Receiver():
         
     def receive(self, server, topic, config, network_reader):
         logging.info("will start the rcv")
-        handler = ReceiverHandler(server,topic, network_reader)
-        container = Container(handler)
-        container.container_id = 'topology.events'
-        container.create_receiver(server, 
-                                  source = topic, 
-                                  name = 'topology_events_container',
-                                  handler = handler,
-                                  context = config)
+        handler = ReceiverHandler(server, topic, config, network_reader)
+        container = Container(handler).run()
 
 
 class ReceiverHandler(MessagingHandler):
-    def __init__(self, server,topic, network_reader):
+    def __init__(self, server,topic, config, network_reader):
         super(ReceiverHandler, self).__init__()
         self.server = server
         self.topic = topic
+        self.config = config
         self.network_reader = network_reader
         logging.info("will start listning for events in the topic: {}".format(self.topic))
+   
+    def on_start(self, event) -> None:
+        conn = event.container.connect(self.server)
+        event.container.create_receiver(conn, self.topic)
 
     def on_message(self, event):
         try:
             received_message = json.loads(event.message.body)
-            config = event.receiver.context
             logging.info('message received {}. On topic {}'.format(received_message, self.topic))
-            if received_message['action'] == 'CREATED' or received_message['action'] == 'UPDATE' :
-                config.target_nodes[received_message['content']['name']] = received_message['content']['mgmtIP']
-                # launch a network read, probably need to pass network reader as argument
-                self.network_reader.read(config)
-            elif received_message['action'] == 'DELETE':
-                config.target_nodes.pop(received_message['content']['name'])
-
+            self.process_message(received_message)
         except Exception:
             traceback.print_exc()
+
+    def process_message(self, message):
+        if message['action'] == 'CREATED' or message['action'] == 'UPDATE' :
+            self.config.target_nodes[message['content']['name']] = message['content']['mgmtIP']
+            # launch a network read, probably need to pass network reader as argument
+            self.network_reader.read(self.config)
+        elif message['action'] == 'DELETE':
+            self.config.target_nodes.pop(message['content']['name'])
 
