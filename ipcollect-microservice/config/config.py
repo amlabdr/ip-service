124d71d146b96744c1712018670161fe01057bc5
mhenimerz <hani.merzouki@gmail.com>
2023-03-22 15:27:07 -0400

    switch from pydot to json object for network targets

--- ipcollect-microservice/config/config.py
+++ ipcollect-microservice/config/config.py
@@ -1,18 +1,15 @@
+import json
 import os, logging
-import pydot
 
 class Config:
     def __init__(self):
         self.repeat_timer = os.environ.get('REPEAT_TIMER','10')
         self.controller_url = os.environ.get('CONTROLLER_URL','http://10.11.200.125:8787')
         self.network_targets_file_path = os.environ.get('NET_TARGETS', 'ipcollect-microservice/config/network_targets.dot')
-        self.network_targets = self.read_network_targets()
+        self.load_nodes()
 
-    def read_network_targets(self):
-        '''reads the network topology file and loads it to memory as a dictionary using pydot'''
-        try:
-            graph = pydot.graph_from_dot_file(self.network_targets_file_path)
-            return graph[0]
-        except IOError:
-            logging.error("*********** ERROR reading network targets file **********")
-            exit(1)
+    def load_nodes(self):
+        with open(self.network_targets_file_path) as f:
+            network_targets = f.read()
+            self.network_targets = json.loads(network_targets)
+            logging.info('reading network targets from file successful')
