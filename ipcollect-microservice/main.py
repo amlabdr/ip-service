c4bc14add2aaf32fccf097b26000c34850e818b5
mhenimerz <hani.merzouki@gmail.com>
2023-05-23 17:09:16 +0000

    get network targets from the controller

--- ipcollect-microservice/main.py
+++ ipcollect-microservice/main.py
@@ -6,27 +6,24 @@ from threading import Thread
 
 def run():
 
-    ctrl = ControllerService('10.11.200.125:5672')
-    authentication_period = 60 ## or read it from external file
-    thread_authentification = Thread(target=ctrl.controllerAuthentication,args=(authentication_period,))
-    thread_authentification.start()
-    time.sleep(1)
-    #get subnets
-    subnets = ctrl.get(url="http://10.11.200.125:8787/api/topology/subnet/type/QNET")
-
-    print(subnets)
-    return
-
     config = Config()
+    ctrl = ControllerService(config)
+    
+    # 1. get subnets
+    subnets = ctrl.get(url="http://10.11.200.125:8787/api/topology/subnet/type/QNET")
+    # 2. get target nodes by subnet_id
+    config.network_targets = ctrl.get(url='http://10.11.200.125:8787/api/topology/'+subnet.id+'/nodes')
+    # 3. start periodic reader thread
     result = {}
     reader = Reader() 
-    reader.read(config) # read from topology service (HTTP: login (return token), get subnets (with token), pick up one subnet (dc-qnet, id), read nodes w/ subent Id -> target_nodes)
+    periodic_collection_thread = Thread(target=reader.read, args=(config))
     result = reader.result
     json_data = json.dumps(result,indent=2)
-    with open("/tmp/result.json", 'w') as json_file:
+    with open("/tmp/result.json", 'w') as json_file:ZZ
         json_file.write(json_data)
         json_file.close()
     
+    # 4. subscribe to topology events in a seperate thread
     ctrl.publish_collected_topology(topic = 'topic://topology.collection', message = result)
     ctrl.subcribe_to_topology_events(topic = 'topic://topology.events',
                                      config = config,
@@ -35,5 +32,4 @@ def run():
     
 if __name__ == '__main__':
     run()
-
 
