707c6e00eb8cf77f71f91f87c8469431b68ecf59
mhenimerz <hani.merzouki@gmail.com>
2023-05-23 13:07:23 +0000

    add env variables to config object

--- ipcollect-microservice/config/config.py
+++ ipcollect-microservice/config/config.py
@@ -3,8 +3,13 @@ import os, logging
 
 class Config:
     def __init__(self):
-        self.repeat_timer = os.environ.get('REPEAT_TIMER','10')
-        self.controller_url = os.environ.get('CONTROLLER_URL','http://10.11.200.125:8787')
+        self.repeat_timer = os.environ.get('COLLECTION_REPEAT_TIMER','60')
+        self.controller_ip = os.environ.get('CONTROLLER_IP','10.11.200.125')
+        self.controller_rest_port = os.environ.get('CONTROLLER_REST_PORT','8787')
+        self.controller_amqp_port = os.environ.get('CONTROLLER_AMQP_PORT','5672')
+        self.controller_rest_username = os.environ.get('CONTROLLER_REST_USERNAME', 'admin')
+        self.controller_rest_password = os.environ.get('CONTROLLER_REST_PASSWORD', 'admin')
+        
         self.network_targets_file_path = os.environ.get('NET_TARGETS', 'config/network_targets.dot')
         self.load_nodes()
 
