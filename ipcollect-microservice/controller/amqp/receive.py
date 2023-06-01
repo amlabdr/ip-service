eb6834010c1c34d319895c2811c180f324e5f4ec
mhenimerz <hani.merzouki@gmail.com>
2023-06-01 14:07:28 -0400

    pass controller to event receiver

--- ipcollect-microservice/controller/amqp/receive.py
+++ ipcollect-microservice/controller/amqp/receive.py
@@ -16,21 +16,21 @@ class Receiver():
     def __init__(self):
         super(Receiver, self).__init__()
         
-    def receive(self, server, topic, config, network_reader):
+    def receive(self, server, topic, config, network_reader, ctrl):
         print("will start the receiver")
-        logging.info("will start the receiver")
-        handler = ReceiverHandler(server, topic, config, network_reader)
+        handler = ReceiverHandler(server, topic, config, network_reader, ctrl)
         container = Container(handler).run()
 
 
 class ReceiverHandler(MessagingHandler):
-    def __init__(self, server,topic, config, network_reader):
+    def __init__(self, server,topic, config, network_reader, ctrl):
         super(ReceiverHandler, self).__init__()
         self.server = server
         self.topic = topic
         self.config = config
         self.network_reader = network_reader
-        logging.info("will start listning for events in the topic: {}".format(self.topic))
+        self.ctrl = ctrl
+        print("will start listning for events in the topic: {}".format(self.topic))
    
     def on_start(self, event) -> None:
         conn = event.container.connect(self.server)
