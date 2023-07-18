e740627aba9f89ca9b0225ff84201ecba08a1c91
mhenimerz <hani.merzouki@gmail.com>
2023-07-18 08:29:06 -0400

    add main ip address

--- ipcollect-microservice/net/readers/metadata_reader.py
+++ ipcollect-microservice/net/readers/metadata_reader.py
@@ -14,11 +14,20 @@ class MetadataReader():
             if interface["NAME"] == "eth0":
                 return interface["MACADDR"]
 
+    # get mac address of the management interface which is the ID of the device
+    def get_loopback_address(self):
+        iface_reader = InterfaceReader(self.interface_dict)
+        iface_reader.read()
+        for interface in iface_reader.result:
+            if interface["NAME"] == "lo":
+                return interface["IPADDR"]
+    
     def __str__(self) -> str:
         return f'MetadataReader = {vars(self)}'
 
     def read(self):
         self.result["HOSTNAME"] = self.metadata_dict["system"]["state"]["hostname"]
+        self.result["MAIN_ADDRESS"] = self.get_loopback_address() 
         self.result["MAC"] = self.get_mac()
         self.result["TYPE"] = "ROUTER"
         self.result["PLATFORM"] = "OcNOS"
