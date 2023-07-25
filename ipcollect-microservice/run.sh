d3b4b63e87dfbfd1b59cb73819b601e1c38fdcf1
mhenimerz <hani.merzouki@gmail.com>
2023-07-25 10:11:33 -0400

    add docker run script

---
+++ ipcollect-microservice/run.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+set -e
+
+DOCKERHUB_USERNAME="multiversenms"
+TAG="latest"
+BUILD_IMAGE=false
+RUN_IMAGE=false
+PUSH_IMAGE=false
+
+usage() {
+    echo "Usage: $0 [-b] (-r or -p)"
+    exit 1
+}
+
+# Parse command-line options
+while getopts ":brp" opt; do
+    case $opt in
+        b)
+            BUILD_IMAGE=true
+            ;;
+        r)
+            RUN_IMAGE=true
+            ;;
+        p)
+            PUSH_IMAGE=true
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG"
+            usage
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument."
+            usage
+            ;;
+    esac
+done
+
+
+# Check if both -r and -p flags are used together
+if [ "$RUN_IMAGE" = false ] && [ "$PUSH_IMAGE" = false ]; then
+    echo "ERROR: Missing -r (run) or -p (push) flag."
+    usage
+fi
+
+# Check if both -r and -p flags are used together
+if [ "$RUN_IMAGE" = true ] && [ "$PUSH_IMAGE" = true ]; then
+    echo "ERROR: -r (run) and -p (push) flags cannot be used together."
+    usage
+fi
+
+# Log in to Docker Hub
+if [ "$PUSH_IMAGE" = true ]; then
+    docker login --username $DOCKERHUB_USERNAME
+fi
+
+# Build image
+if [ "$BUILD_IMAGE" = true ]; then
+    docker build -t "multiverse-ipcollect" .
+fi
+
+# Push the tagged image to Docker Hub
+if [ "$PUSH_IMAGE" = true ]; then
+    docker tag multiverse-gui multiversenms/ipcollect:latest
+    docker push multiversenms/ipcollect:latest
+fi
+
+# Run the local image
+if [ "$RUN_IMAGE" = true ]; then
+    docker run -i --rm --name "multiverse-ipcollect" -p "8071:8071" -e "CONTROLLER=10.11.200.125" "multiverse-ipcollect"
+fi
+
+# Log out from Docker Hub
+if [ "$PUSH_IMAGE" = true ]; then
+    docker logout
+fi
