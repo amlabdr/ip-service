103f910ca11624678dadaea77f1ee9d73bea4ba8
Abderrahim <amalou.abdr@gmail.com>
2023-07-26 10:51:44 -0400

    fixing amqp broker address

--- ipconfig-microservice/config/config.py
+++ ipconfig-microservice/config/config.py
@@ -5,6 +5,7 @@ class Config:
     def __init__(self):
         self.controller_ip = os.environ.get('CONTROLLER','10.11.200.125')
         self.controller_rest_port = os.environ.get('CONTROLLER_REST_PORT','8787')
+        self.amqp_broker = os.environ.get('AMQP_BROKER','activemq')
         self.controller_amqp_port = os.environ.get('CONTROLLER_AMQP_PORT','5672')
         self.controller_rest_username = os.environ.get('CONTROLLER_REST_USERNAME', 'admin')
         self.controller_rest_password = os.environ.get('CONTROLLER_REST_PASSWORD', 'admin')
